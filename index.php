9a2628b7eeea3ca417a32f0db3a229a39d11a44b
biscuitsaler <durandmallory@live.fr>
2017-01-09 14:40:31 +0100

    Version fred

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <?php
-class Model 
+class Model
 {
     public $string;
 
-    public function __construct()
-    {
-        $this -> string = "Clique-moi, grand fou!";
+    public function __construct() {
+        $this -> string = "Clique-moi, grand fou !";
     }
+
+    /*public function clicked() {
+        $this -> string = "Clique-moi, grand fou !";
+    }
+
+
+    public function clickedAgain() {
+        $this -> string = "Ich bin clicked again !";
+    }
+
+    public function clickedAgainAndAgain() {
+        $this -> string = "Ich bin clicked again and again!";
+    }*/
 }
+
 class View
 {
     private $model;
@@ -16,27 +29,42 @@ class View
     public function __construct($controller, $model) {
         $this -> controller = $controller;
         $this -> model = $model;
-
     }
+
     public function output() {
-        return "<p><a href=\"index.php?action=clicked\">" . $this -> model -> string ."</a></p>";
+        //$url = "<p><a href=\"index.php\">Back to the home</a></p>";
+        $url = "<p><a href=\"index.php?action=clicked\">" . $this -> model -> string . "</a></p>";
+        return $url;
     }
 }
-class Controller 
+
+class Controller
 {
     private $model;
+
     public function __construct($model) {
-        $this-> model = $model;
+        $this -> model = $model;
     }
     public function clicked() {
-        $this -> model -> string = "Ich bin updat&eacute;";
+        $this -> model -> string = "Méthode clicked appelee";
+    }/*
+
+    public function clickedAgain($model) {
+        $this -> model = $model;
     }
+
+    public function clickedAgainAndAgain($model) {
+        $this -> model = $model;
+    }
+*/
 }
+
 $model = new Model();
 $controller = new Controller($model);
 $view = new View($controller, $model);
-if (isset($_GET['action']) && !empty($_GET['action'])) {
-    $controller -> {$_GET['action']} ();
-}
-echo $view -> output();
 
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $controller -> {$_GET['action']}();
+}
+
+echo $view -> output();
